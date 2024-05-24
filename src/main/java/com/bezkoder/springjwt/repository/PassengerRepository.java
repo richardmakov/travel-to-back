4b364b10f856246671ca313695efac7353767661
Richard Makovs <richard.makovs@movicoders.com>
2024-05-24 14:51:28 +0200

    new updates 2

--- src/main/java/com/bezkoder/springjwt/repository/PassengerRepository.java
+++ src/main/java/com/bezkoder/springjwt/repository/PassengerRepository.java
@@ -3,4 +3,5 @@ package com.bezkoder.springjwt.repository;
 import com.bezkoder.springjwt.models.Booking_passengers;
 import org.springframework.data.jpa.repository.JpaRepository;
 public interface PassengerRepository extends JpaRepository<Booking_passengers, Long> {
+
 }
